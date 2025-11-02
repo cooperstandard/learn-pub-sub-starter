0335fb9079b988a85f3ded10fa133b126b49150c
Cooper Standard <cooper@cooperstandard.org>
2025-11-02 12:29:51 -0800

    client game loop

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
@@ -31,10 +29,39 @@ func main() {
 	if err != nil {
 		log.Fatalf("unable to bind q and channel")
 	}
+	gameState := gamelogic.NewGameState(username)
+
+	fmt.Printf("gameState: %v\n", gameState)
+	for {
+		input := gamelogic.GetInput()
+		if input[0] == "quit" {
+			break
+		} else if input[0] == "help" {
+			gamelogic.PrintClientHelp()
+		} else if input[0] == "spam" {
+			fmt.Println("spamming not allowed yet")
+		} else if input[0] == "status" {
+			gameState.CommandStatus()
+		} else if input[0] == "spawn" {
+			err := gameState.CommandSpawn(input)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else if input[0] == "move" {
+			move, err := gameState.CommandMove(input)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println(move)
+		} else {
+			fmt.Println("invalid command")
+		}
+	}
 
 	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
-	fmt.Println("signal received")
+	// signalChan := make(chan os.Signal, 1)
+	// signal.Notify(signalChan, os.Interrupt)
+	// <-signalChan
+	// fmt.Println("signal received")
 }
